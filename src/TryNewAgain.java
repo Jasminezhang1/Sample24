a764a034e19eabed0ef437c0b13f03ecbdfff764
Jasminezhang1 <fenzhang1211@gmail.com>
2024-04-06 21:33:59 -0400

    this is  Trynewagain files

---
+++ src/TryNewAgain.java
@@ -0,0 +1,10 @@
+public class TryNewAgain {
+    public static void main(String[] args){
+        int number1 = 1;
+        int number2 = 2;
+        double average = (number2 +number1)/2.0;
+        System.out.println(average);
+
+
+    }
+}
